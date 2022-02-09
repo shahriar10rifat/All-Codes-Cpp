251050c4abae30cc1295aee0b74a114eb33e6b54
ShahriarRifatJZ <86244928+ShahriarRifatJZ@users.noreply.github.com>
2022-02-10 03:13:11 +0600

    Update Seive.cpp

--- Seive.cpp
+++ Seive.cpp
@@ -1,14 +1,11 @@
-//..........prime Numbers.............
-/*#include <bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 bool mark[1007];
 void seive (int n){
     mark[0] = mark[1] = 1;
 
-    for(int i=2; i<=n; i+=2) mark[i]=1;
-
-    for(int i=3; i<n; i+=2){
+    for(int i=2; i<n; i+=2){
         if(!mark[i]){
             for(int j=i+i; j<=n; j+=i){
                 mark[j]=1;
@@ -17,19 +14,16 @@ void seive (int n){
     }
 }
 int main(){
-    seive(1000);
-    for(int i=1; i<=1000; i++){
+    cout<<"Enter a Element: ";
+    int n; cin>>n;
+    seive(n);
+    for(int i=1; i<=n; i++){
         if(!mark[i]){
             cout<<"Prime number is :"<<i<<endl;
         }
     }
     return 0;
 }
-*/
-///..........prime Factorization ...............
-#include <bits/stdc++.h>
-using namespace std;
-
 
 
 
