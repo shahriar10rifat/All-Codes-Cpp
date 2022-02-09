9e312f05e197eeeb06400f0fc705b16b4cf2fa35
ShahriarRifatJZ <86244928+ShahriarRifatJZ@users.noreply.github.com>
2022-02-10 03:31:21 +0600

    Update PrimeNumber.cpp

--- PrimeNumber.cpp
+++ PrimeNumber.cpp
@@ -1,28 +1,33 @@
-/*Prime number
-#include <bits/stdc++.h>
+/*
+///............Prime number
+#include<bits/stdc++.h>
 using namespace std;
-int main()
-{
-  int n, i, m=0, prime=0;
-  cout << "Enter the Number to check Prime: ";
-  cin >> n;
-  m=n/2;
-  for(i = 2; i <= m; i++)
-  {
-      if(n % i == 0)
-      {
-          cout<<"Number is not Prime."<<endl;
-          prime=1;
-          break;
-      }
+
+int main() {
+    int n, i, prime = 0;
+    printf("Enter a Number: ");
+    scanf("%d", &n);
+
+    for(int i=2; i<=sqrt(n); ++i) {
+        if (n%i == 0) {
+        prime = 1;
+        break;
+        }
+    }
+    if (n==0 || n==1) {
+    cout<<n<<" is neither prime nor composite."<<endl;
+    }
+    else {
+
+    if (prime == 0)
+        cout<<n<<" is a prime number."<<endl;
+    else
+      cout<<n<<" is not a prime number."<<endl;
   }
-  if (prime==0)
-      cout << "Number is Prime."<<endl;
   return 0;
 }
 */
-/*_____________________________________________
-prime_o(n)
+/*______________prime_o(n)
 #include <bits/stdc++.h>
 using namespace std;
 bool prime (int a){
